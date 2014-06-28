4906b31fdfe17a7d56c2617186f5f293c938a6a5
dakkanner <dakkanner@gmail.com>
2014-06-27 22:35:58 -0700

    First page of GUI plus the help page created.

--- duty_schedule/Form1.Designer.cs
+++ duty_schedule/Form1.Designer.cs
@@ -29,19 +29,9 @@
         private void InitializeComponent()
         {
             this.buttonP1Next = new System.Windows.Forms.Button();
-            this.textBoxTitle1 = new System.Windows.Forms.TextBox();
-            this.textBoxTable1_0 = new System.Windows.Forms.TextBox();
-            this.textBoxTable1_1 = new System.Windows.Forms.TextBox();
-            this.textBoxTable1_2 = new System.Windows.Forms.TextBox();
-            this.textBoxTable1_3 = new System.Windows.Forms.TextBox();
-            this.textBoxTable1_4 = new System.Windows.Forms.TextBox();
-            this.textBoxTable1_5 = new System.Windows.Forms.TextBox();
-            this.textBoxTable1_6 = new System.Windows.Forms.TextBox();
-            this.textBoxTable1_7 = new System.Windows.Forms.TextBox();
-            this.textBoxTable1_8 = new System.Windows.Forms.TextBox();
-            this.textBoxTable1_9 = new System.Windows.Forms.TextBox();
-            this.table1 = new System.Windows.Forms.TableLayoutPanel();
-            this.table1.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // buttonP1Next
@@ -55,126 +45,53 @@
             this.buttonP1Next.UseVisualStyleBackColor = true;
             this.buttonP1Next.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBoxTitle1
+            // label1
             // 
-            this.textBoxTitle1.Location = new System.Drawing.Point(20, 50);
-            this.textBoxTitle1.Name = "textBoxTitle1";
-            this.textBoxTitle1.Size = new System.Drawing.Size(170, 20);
-            this.textBoxTitle1.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Would you like to use the wizard or pre-made files?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBoxTable1_0
+            // radioButton1
             // 
-            this.textBoxTable1_0.Location = new System.Drawing.Point(3, 3);
-            this.textBoxTable1_0.Name = "textBoxTable1_0";
-            this.textBoxTable1_0.Size = new System.Drawing.Size(144, 20);
-            this.textBoxTable1_0.TabIndex = 0;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Enabled = false;
+            this.radioButton1.Location = new System.Drawing.Point(59, 95);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.Text = "Wizard";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
-            // textBoxTable1_1
+            // radioButton2
             // 
-            this.textBoxTable1_1.Location = new System.Drawing.Point(3, 28);
-            this.textBoxTable1_1.Name = "textBoxTable1_1";
-            this.textBoxTable1_1.Size = new System.Drawing.Size(144, 20);
-            this.textBoxTable1_1.TabIndex = 1;
-            // 
-            // textBoxTable1_2
-            // 
-            this.textBoxTable1_2.Location = new System.Drawing.Point(3, 53);
-            this.textBoxTable1_2.Name = "textBoxTable1_2";
-            this.textBoxTable1_2.Size = new System.Drawing.Size(144, 20);
-            this.textBoxTable1_2.TabIndex = 2;
-            // 
-            // textBoxTable1_3
-            // 
-            this.textBoxTable1_3.Location = new System.Drawing.Point(3, 78);
-            this.textBoxTable1_3.Name = "textBoxTable1_3";
-            this.textBoxTable1_3.Size = new System.Drawing.Size(144, 20);
-            this.textBoxTable1_3.TabIndex = 3;
-            // 
-            // textBoxTable1_4
-            // 
-            this.textBoxTable1_4.Location = new System.Drawing.Point(3, 103);
-            this.textBoxTable1_4.Name = "textBoxTable1_4";
-            this.textBoxTable1_4.Size = new System.Drawing.Size(144, 20);
-            this.textBoxTable1_4.TabIndex = 4;
-            // 
-            // textBoxTable1_5
-            // 
-            this.textBoxTable1_5.Location = new System.Drawing.Point(3, 128);
-            this.textBoxTable1_5.Name = "textBoxTable1_5";
-            this.textBoxTable1_5.Size = new System.Drawing.Size(144, 20);
-            this.textBoxTable1_5.TabIndex = 5;
-            // 
-            // textBoxTable1_6
-            // 
-            this.textBoxTable1_6.Location = new System.Drawing.Point(3, 153);
-            this.textBoxTable1_6.Name = "textBoxTable1_6";
-            this.textBoxTable1_6.Size = new System.Drawing.Size(144, 20);
-            this.textBoxTable1_6.TabIndex = 6;
-            // 
-            // textBoxTable1_7
-            // 
-            this.textBoxTable1_7.Location = new System.Drawing.Point(3, 178);
-            this.textBoxTable1_7.Name = "textBoxTable1_7";
-            this.textBoxTable1_7.Size = new System.Drawing.Size(144, 20);
-            this.textBoxTable1_7.TabIndex = 7;
-            // 
-            // textBoxTable1_8
-            // 
-            this.textBoxTable1_8.Location = new System.Drawing.Point(3, 203);
-            this.textBoxTable1_8.Name = "textBoxTable1_8";
-            this.textBoxTable1_8.Size = new System.Drawing.Size(144, 20);
-            this.textBoxTable1_8.TabIndex = 8;
-            // 
-            // textBoxTable1_9
-            // 
-            this.textBoxTable1_9.Location = new System.Drawing.Point(3, 228);
-            this.textBoxTable1_9.Name = "textBoxTable1_9";
-            this.textBoxTable1_9.Size = new System.Drawing.Size(144, 20);
-            this.textBoxTable1_9.TabIndex = 9;
-            // 
-            // table1
-            // 
-            this.table1.ColumnCount = 1;
-            this.table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.table1.Controls.Add(this.textBoxTable1_0, 0, 0);
-            this.table1.Controls.Add(this.textBoxTable1_1, 0, 1);
-            this.table1.Controls.Add(this.textBoxTable1_2, 0, 2);
-            this.table1.Controls.Add(this.textBoxTable1_3, 0, 3);
-            this.table1.Controls.Add(this.textBoxTable1_4, 0, 4);
-            this.table1.Controls.Add(this.textBoxTable1_5, 0, 5);
-            this.table1.Controls.Add(this.textBoxTable1_6, 0, 6);
-            this.table1.Controls.Add(this.textBoxTable1_7, 0, 7);
-            this.table1.Controls.Add(this.textBoxTable1_8, 0, 8);
-            this.table1.Controls.Add(this.textBoxTable1_9, 0, 9);
-            this.table1.Location = new System.Drawing.Point(30, 100);
-            this.table1.Name = "table1";
-            this.table1.RowCount = 10;
-            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
-            this.table1.Size = new System.Drawing.Size(150, 250);
-            this.table1.TabIndex = 2;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(59, 118);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(91, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Pre-made files";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 461);
-            this.Controls.Add(this.table1);
-            this.Controls.Add(this.textBoxTitle1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonP1Next);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.table1.ResumeLayout(false);
-            this.table1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,19 +100,9 @@
         #endregion
 
         private System.Windows.Forms.Button buttonP1Next;
-        private System.Windows.Forms.TextBox textBoxTitle1;
-
-        private System.Windows.Forms.TextBox textBoxTable1_0;
-        private System.Windows.Forms.TextBox textBoxTable1_1;
-        private System.Windows.Forms.TextBox textBoxTable1_2;
-        private System.Windows.Forms.TextBox textBoxTable1_3;
-        private System.Windows.Forms.TextBox textBoxTable1_4;
-        private System.Windows.Forms.TextBox textBoxTable1_5;
-        private System.Windows.Forms.TextBox textBoxTable1_6;
-        private System.Windows.Forms.TextBox textBoxTable1_7;
-        private System.Windows.Forms.TextBox textBoxTable1_8;
-        private System.Windows.Forms.TextBox textBoxTable1_9;
-        private System.Windows.Forms.TableLayoutPanel table1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
 
